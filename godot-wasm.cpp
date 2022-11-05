e1e039741076f22322333a9374863746bb08ad4a
Ashton Meuser <ashtonmeuser@gmail.com>
2022-11-05 11:13:00 -0700

    Initial commit

---
+++ godot-wasm.cpp
@@ -0,0 +1,102 @@
+#include "godot-wasm.h"
+
+using namespace godot;
+
+void Wasm::_register_methods() {
+  register_method("load", &Wasm::load);
+  register_method("fire", &Wasm::fire);
+}
+
+Wasm::Wasm() {
+  engine = wasm_engine_new();
+  store = wasm_store_new(engine);
+  module = NULL;
+  instance = NULL;
+  names = Dictionary();
+}
+
+Wasm::~Wasm() {
+  if (instance) wasm_instance_delete(instance);
+  if (module) wasm_module_delete(module);
+  if (store != nullptr) wasm_store_delete(store);
+  if (engine != nullptr) wasm_engine_delete(engine);
+}
+
+void Wasm::_init() { }
+
+godot_error Wasm::load(PoolByteArray bytecode) {
+  // Load binary
+  wasm_byte_vec_t wasm_bytes;
+  wasm_byte_vec_new_uninitialized(&wasm_bytes, bytecode.size());
+  for (int i = 0; i < bytecode.size(); i++) wasm_bytes.data[i] = bytecode[i];
+
+  // Validate binary
+  ERR_FAIL_COND_V(!wasm_module_validate(store, &wasm_bytes), GODOT_ERR_INVALID_DATA);
+
+  // Compile
+  module = wasm_module_new(store, &wasm_bytes);
+  wasm_byte_vec_delete(&wasm_bytes);
+  ERR_FAIL_COND_V(!module, GODOT_ERR_COMPILATION_FAILED);
+
+  // Instantiate
+  wasm_extern_vec_t imports = WASM_EMPTY_VEC;
+  instance = wasm_instance_new(store, module, &imports, NULL);
+  ERR_FAIL_COND_V(!instance, GODOT_ERR_CANT_CREATE);
+
+  // Map names to export indices
+  names = map_names(module);
+
+  return GODOT_OK;
+}
+
+Variant Wasm::fire(String name, Array args) {
+  // Validate function name
+  ERR_FAIL_COND_V(!names.has(name), Variant());
+
+  // Retrieve exports
+  wasm_extern_vec_t exports;
+  wasm_instance_exports(instance, &exports);
+  const wasm_func_t* func = wasm_extern_as_func(exports.data[(int)names[name]]);
+  ERR_FAIL_COND_V(!func, Variant());
+
+  // Construct args
+  std::vector<wasm_val_t> vect;
+  for (int i = 0; i < args.size(); i++) {
+    Variant val = args[i];
+    switch (val.get_type()) {
+      case Variant::INT:
+        vect.push_back(WASM_I64_VAL((int64_t)val));
+        break;
+      case Variant::REAL:
+        vect.push_back(WASM_F64_VAL((float64_t)val));
+        break;
+      default:
+        ERR_FAIL_V(Variant());
+    }
+  }
+
+  // Call function
+  wasm_val_t results_val[1] = { WASM_INIT_VAL };
+  wasm_val_vec_t f_args = {vect.size(), vect.data()};
+  wasm_val_vec_t f_results = WASM_ARRAY_VEC(results_val);
+  ERR_FAIL_COND_V(wasm_func_call(func, &f_args, &f_results), Variant());
+
+  // Extract result
+  wasm_val_t result = results_val[0];
+  switch (result.kind) {
+    case WASM_I64: return Variant(result.of.i64);
+    case WASM_F64: return Variant(result.of.f64);
+    default: ERR_FAIL_V(Variant());
+  }
+}
+
+Dictionary Wasm::map_names(wasm_module_t* module) {
+  wasm_exporttype_vec_t exports;
+  wasm_module_exports(module, &exports);
+  Dictionary dict;
+  for (int i = 0; i < exports.size; i++) {
+    const wasm_name_t* name = wasm_exporttype_name(exports.data[i]);
+    dict[String(name->data)] = i;
+  }
+  return dict;
+}
---
+++ godot-wasm.h
@@ -0,0 +1,30 @@
+#ifndef GODOT_WASM_H
+#define GODOT_WASM_H
+
+#include <vector>
+#include <Godot.hpp>
+#include "wasmer.h"
+
+namespace godot {
+class Wasm : public Reference {
+    GODOT_CLASS(Wasm, Reference)
+
+private:
+    wasm_engine_t* engine;
+    wasm_store_t* store;
+    wasm_module_t* module;
+    wasm_instance_t* instance;
+    Dictionary names;
+    static Dictionary map_names(wasm_module_t* module);
+
+public:
+    static void _register_methods();
+    Wasm();
+    ~Wasm();
+    void _init();
+    godot_error load(PoolByteArray bytecode);
+    Variant fire(String name, Array args);
+};
+}
+
+#endif
