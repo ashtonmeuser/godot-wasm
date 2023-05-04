38ca45e2d7bf8fa13bdae3c33175223607b6050a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-05-04 01:42:02 -0700

    Add doc classes.
    Needs description.

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
 def can_build(env, platform):
-    return platform in ['linux', 'linuxbsd', 'windows', 'osx', 'macos']
+    return platform in ["linux", "linuxbsd", "windows", "osx", "macos"]
+
 
 def configure(env):
     pass
+
+
+def get_doc_classes():
+    return [
+        "Wasm",
+        "StreamPeerWasm",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
