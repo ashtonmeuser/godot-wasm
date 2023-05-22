6fe5262be093b8bd77e35773cbe37af70df60315
Ashton Meuser <ashtonmeuser@gmail.com>
2023-05-22 16:14:31 -0700

    Update examples

--- examples/wasm-test/utils/GodotWasmTestSuite.gd
+++ examples/wasm-test/utils/GodotWasmTestSuite.gd
@@ -1,25 +1,28 @@
 extends TestSuite
 class_name GodotWasmTestSuite
 
+const TYPE_FLOAT: int = TYPE_REAL
 const PAGE_SIZE: int = 0b1 << 16
 const PAGES_MAX: int = PAGE_SIZE * (PAGE_SIZE - 1)
 
 # Test suite overrides
 
 func expect_error(s: String):
-	super.expect_error("(Godot Wasm: )?" + s)
+	.expect_error("(Godot Wasm: )?" + s)
 
 # General utils
 
-func load_wasm(f: String, imports: Dictionary = {}, e: Error = OK) -> Wasm:
+func load_wasm(f: String, imports: Dictionary = {}, e: int = OK) -> Wasm:
 	var wasm = Wasm.new()
 	var buffer = read_file(f)
 	var error = wasm.load(buffer, imports)
 	expect_eq(error, e)
 	return wasm
 
-func read_file(f: String) -> PackedByteArray:
-	return FileAccess.get_file_as_bytes("res://wasm/%s.wasm" % f)
+func read_file(f: String) -> PoolByteArray:
+	var file = File.new()
+	file.open("res://wasm/%s.wasm" % f, File.READ)
+	return file.get_buffer(file.get_len())
 
 # Dummy import to supply to Wasm modules
 static func _dummy_import(a = "", b = "", c = "", d = ""):
@@ -31,7 +34,7 @@ func dummy_import() -> Array:
 	return [self, "_dummy_import"]
 
 func make_bytes(data: Array):
-	return PackedByteArray(data)
+	return PoolByteArray(data)
 
 func get_cmdline_user_args() -> Array:
-	return OS.get_cmdline_user_args()
+	return []
