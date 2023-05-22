cbbf4549b3a90a7028d99ed077bff1ab9ad3d76f
Ashton Meuser <ashtonmeuser@gmail.com>
2023-05-22 16:13:34 -0700

    Godot 3 support

--- register_types.cpp
+++ register_types.cpp
@@ -1,36 +1,15 @@
+#ifdef GODOT_MODULE
+
 #include "register_types.h"
+#include "core/class_db.h"
 #include "src/wasm.h"
 #include "src/wasm-memory.h"
 
-using namespace godot;
-
-void initialize_wasm_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-    return;
-  }
-
-  ClassDB::register_class<Wasm>();
-  ClassDB::register_class<WasmMemory>();
+void register_wasm_types() {
+    ClassDB::register_class<godot::Wasm>();
+    ClassDB::register_class<godot::WasmMemory>();
 }
 
-void uninitialize_wasm_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-    return;
-  }
-}
-
-#ifndef GODOT_MODULE
-
-extern "C" {
-  GDExtensionBool GDE_EXPORT wasm_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-
-    init_obj.register_initializer(initialize_wasm_module);
-    init_obj.register_terminator(uninitialize_wasm_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-  }
-}
+void unregister_wasm_types() { }
 
 #endif
--- register_types.h
+++ register_types.h
@@ -1,18 +1,7 @@
 #ifndef GODOT_WASM_REGISTER_TYPES_H
 #define GODOT_WASM_REGISTER_TYPES_H
 
-// This define is only needed because the GODOT_MODULE define isn't detected here
-// May be related to https://github.com/godotengine/godot/issues/75914
-#ifdef GDEXTENSION
-  #define NS godot
-  #include <godot_cpp/core/class_db.hpp>
-#else
-  #define NS
-  #include "modules/register_module_types.h"
-  #include "core/object/class_db.h"
-#endif
-
-void initialize_wasm_module(NS::ModuleInitializationLevel p_level);
-void uninitialize_wasm_module(NS::ModuleInitializationLevel p_level);
+void register_wasm_types();
+void unregister_wasm_types();
 
 #endif
