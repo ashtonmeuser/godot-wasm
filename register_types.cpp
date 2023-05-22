b8c8762af9e1b23db610823e4dfbf0c28ae4c3da
Ashton Meuser <ashtonmeuser@gmail.com>
2023-05-22 16:13:34 -0700

    Godot 3 support

--- register_types.cpp
+++ register_types.cpp
@@ -1,36 +1,15 @@
+#ifdef GODOT_MODULE
+
 #include "register_types.h"
+#include "core/class_db.h"
 #include "src/godot-wasm.h"
 #include "src/stream-peer-wasm.h"
 
-using namespace godot;
-
-void initialize_wasm_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-    return;
-  }
-
-  ClassDB::register_class<Wasm>();
-  ClassDB::register_class<StreamPeerWasm>();
+void register_wasm_types() {
+    ClassDB::register_class<godot::Wasm>();
+    ClassDB::register_class<godot::StreamPeerWasm>();
 }
 
-void uninitialize_wasm_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-    return;
-  }
-}
-
-#ifndef GODOT_MODULE
-
-extern "C" {
-  GDExtensionBool GDE_EXPORT wasm_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
-
-    init_obj.register_initializer(initialize_wasm_module);
-    init_obj.register_terminator(uninitialize_wasm_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-  }
-}
+void unregister_wasm_types() { }
 
 #endif
--- src/defs.h
+++ src/defs.h
@@ -4,9 +4,8 @@
 #ifdef GODOT_MODULE // Godot includes when building module
   #include "core/io/stream_peer.h"
 #else // Godot addon includes
-  #include "godot_cpp/classes/ref_counted.hpp"
-  #include "godot_cpp/classes/stream_peer_extension.hpp"
-  #include "godot_cpp/variant/utility_functions.hpp"
+  #include <Godot.hpp>
+  #include <StreamPeerGDNative.hpp>
 #endif
 
 #ifdef GODOT_MODULE
@@ -15,15 +14,23 @@
   #define PRINT_ERROR(message) print_error("Godot Wasm: " + String(message))
   #define REGISTRATION_METHOD _bind_methods
 #else
-  #define PRINT(message) UtilityFunctions::print(String(message))
-  #define PRINT_ERROR(message) _err_print_error(__FUNCTION__, __FILE__, __LINE__, "Godot Wasm: " + String(message))
-  #define godot_error Error
-  #define REGISTRATION_METHOD _bind_methods
+  #define OK GODOT_OK
+  #define ERR_INVALID_DATA GODOT_ERR_INVALID_DATA
+  #define ERR_COMPILATION_FAILED GODOT_ERR_COMPILATION_FAILED
+  #define ERR_CANT_CREATE GODOT_ERR_CANT_CREATE
+  #define ERR_PARAMETER_RANGE_ERROR GODOT_ERR_PARAMETER_RANGE_ERROR
+  #define PRINT(message) Godot::print(String(message))
+  #define PRINT_ERROR(message) Godot::print_error("Godot Wasm: " + String(message), __func__, __FILE__, __LINE__)
+  #define GDCLASS GODOT_CLASS
+  #define REGISTRATION_METHOD _register_methods
 #endif
+#define FLOAT REAL
+#define RefCounted Reference
+#define PackedByteArray PoolByteArray
 #define FAIL(message, ret) do { PRINT_ERROR(message); return ret; } while (0)
 #define FAIL_IF(cond, message, ret) if (unlikely(cond)) FAIL(message, ret)
-#define INSTANTIATE_REF(ref) ref.instantiate()
-#define BYTE_ARRAY_POINTER(array) array.ptr()
+#define INSTANTIATE_REF(ref) ref.instance()
+#define BYTE_ARRAY_POINTER(array) array.read().ptr()
 #define NULL_VARIANT Variant()
 #define PAGE_SIZE 65536
 
