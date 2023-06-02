cad3159531d1890c7a7db253eae92675326e514d
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2023-06-01 17:56:49 -0700

    Extended WASI support (#40)
    * Fix wasi_snapshot_preview1.fd_write
    
    * Factory factory pattern
    
    * WASI wasi_snapshot_preview1.clock_time_get
    
    * WASI wasi_snapshot_preview1.random_get
    
    * WASI environ_sizes_get, environ_get (stubbed out)
    
    wasi_snapshot_preview1.environ_sizes_get
    wasi_snapshot_preview1.environ_get
    
    * WASI args_sizes_get, args_get
    
    Parses Godot command line args
    
    * Abstract WASI return trap
    
    * Simple permissions model
    
    * Trap invalid arguments
    
    * Validate permissions
    
    * Clock realtime and monotonic
    
    * Support unexported memory
    
    Checking for memory in WASI shim prevents SIGSEGV
    
    * Update example
    
    * Use Godot-version-agnostic string ops
    
    * Missing include
    
    * Add TODOs

--- src/defs.h
+++ src/defs.h
@@ -2,9 +2,15 @@
 #define GODOT_WASM_DEFS_H
 
 #ifdef GODOT_MODULE // Godot includes when building module
+  #include "core/os/os.h"
+  #include "core/os/time.h"
+  #include "core/crypto/crypto.h"
   #include "core/io/stream_peer.h"
 #else // Godot addon includes
   #include "godot_cpp/classes/ref_counted.hpp"
+  #include "godot_cpp/classes/os.hpp"
+  #include "godot_cpp/classes/time.hpp"
+  #include "godot_cpp/classes/crypto.hpp"
   #include "godot_cpp/classes/stream_peer_extension.hpp"
   #include "godot_cpp/variant/utility_functions.hpp"
 #endif
@@ -14,16 +20,21 @@
   #define PRINT(message) print_line(String(message))
   #define PRINT_ERROR(message) print_error("Godot Wasm: " + String(message))
   #define REGISTRATION_METHOD _bind_methods
+  #define RANDOM_BYTES(n) Crypto::create()->generate_random_bytes(n)
 #else
   #define PRINT(message) UtilityFunctions::print(String(message))
   #define PRINT_ERROR(message) _err_print_error(__FUNCTION__, __FILE__, __LINE__, "Godot Wasm: " + String(message))
   #define godot_error Error
   #define REGISTRATION_METHOD _bind_methods
+  #define RANDOM_BYTES(n) Crypto().generate_random_bytes(n)
 #endif
 #define FAIL(message, ret) do { PRINT_ERROR(message); return ret; } while (0)
 #define FAIL_IF(cond, message, ret) if (unlikely(cond)) FAIL(message, ret)
 #define INSTANTIATE_REF(ref) ref.instantiate()
 #define BYTE_ARRAY_POINTER(array) array.ptr()
+#define CMDLINE_ARGS OS::get_singleton()->get_cmdline_user_args()
+#define TIME_REALTIME Time::get_singleton()->get_unix_time_from_system() * 1000000000
+#define TIME_MONOTONIC Time::get_singleton()->get_ticks_usec() * 1000
 #define NULL_VARIANT Variant()
 #define PAGE_SIZE 65536
 
--- src/wasi-shim.cpp
+++ src/wasi-shim.cpp
@@ -1,13 +1,28 @@
 #include <string>
+#include <vector>
 #include <map>
 #include "wasi-shim.h"
 #include "godot-wasm.h"
+
+// See https://github.com/WebAssembly/wasi-libc/blob/main/libc-bottom-half/headers/public/wasi/api.h
+#define __WASI_CLOCKID_REALTIME (UINT32_C(0)) // The clock measuring real time
+#define __WASI_CLOCKID_MONOTONIC (UINT32_C(1)) // The store-wide monotonic clock
+#define __WASI_ERRNO_SUCCESS (UINT16_C(0)) // No error occurred
+#define __WASI_ERRNO_ACCES (UINT16_C(2)) // [sic] Permission denied
+#define __WASI_ERRNO_INVAL (UINT16_C(28)) // Invalid argument
+#define __WASI_ERRNO_IO (UINT16_C(29)) // I/O error
 
 namespace godot {
   namespace {
     struct wasi_io_vector {
       int32_t offset;
       int32_t length;
+    };
+
+    struct wasi_encoded_strings {
+      int32_t count;
+      int32_t length;
+      std::vector<std::string> args;
     };
 
     wasi_io_vector get_io_vector(wasm_memory_t* memory, int32_t offset, int32_t index = 0) {
@@ -17,58 +32,182 @@ namespace godot {
       return iov;
     }
 
+    template <class T> wasi_encoded_strings encode_args(T args) {
+      wasi_encoded_strings encoded = { 0, 0, {} };
+      String incomplete = "";
+      for (auto i = 0; i < args.size(); i++) {
+        String s = args[i];
+        if (!s.begins_with("--")) { // Invalid; may be value for previous key
+          if (incomplete == "") continue; // Ignore garbage
+          s = incomplete + "=" + s; // Value for previous key
+          incomplete = ""; // Reset incomplete key value pair
+        } else { // Valid key or key value pair
+          s = s.substr(2, -1); // Just key or key=value
+          auto parts = s.split("=");
+          if (parts.size() < 2) { // Incomplete; may have subsequent value
+            incomplete = s;
+            continue;
+          }
+          s = parts[0] + "=" + parts[1]; // Have both key and value
+        }
+        std::string bytes = std::string(s.utf8().get_data()) + '\0'; // Null termination
+        encoded.count += 1;
+        encoded.args.push_back(bytes);
+        encoded.length += bytes.length();
+      }
+      return encoded;
+    }
+
+    wasm_trap_t* wasi_result(wasm_val_vec_t* results, int32_t value = __WASI_ERRNO_SUCCESS, const char* message = NULL) {
+      results->data[0].kind = WASM_I32;
+      results->data[0].of.i32 = value;
+      if (value == __WASI_ERRNO_SUCCESS) return NULL;
+      wasm_message_t trap_message;
+      wasm_name_new_from_string_nt(&trap_message, message);
+      return wasm_trap_new(NULL, &trap_message);
+    }
+
     // WASI fd_write: [I32, I32, I32, I32] -> [I32]
     wasm_trap_t* wasi_fd_write(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
-      FAIL_IF(args->size != 4 || results->size != 1, "Invalid call WASI fd_write", NULL);
-      Wasm* gw = (Wasm*)env;
-      wasm_memory_t* memory = gw->stream.ptr()->memory;
+      FAIL_IF(args->size != 4 || results->size != 1, "Invalid arguments fd_write", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      if (!wasm->has_permission("print")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      byte_t* data = wasm_memory_data(memory);
       int32_t fd = args->data[0].of.i32;
-      int32_t offset = args->data[1].of.i32;
-      int32_t length = args->data[2].of.i32;
+      int32_t offset_iov = args->data[1].of.i32;
+      int32_t count_iov = args->data[2].of.i32;
+      int32_t offset_written = args->data[3].of.i32;
       uint32_t written = 0;
-      for (uint16_t i = 0; i < length; i++) {
-        wasi_io_vector iov = get_io_vector(memory, offset, i);
-        byte_t* data = wasm_memory_data(memory) + iov.offset;
-        std::string message = std::string(data, data + iov.length);
+      for (auto i = 0; i < count_iov; i++) {
+        wasi_io_vector iov = get_io_vector(memory, offset_iov, i);
+        std::string message = std::string(data + iov.offset, data + iov.offset + iov.length);
         if (iov.length == 1 && message == "\u000A") continue; // Skip line feed
         fd == 1 ? PRINT(message.c_str()) : PRINT_ERROR(message.c_str());
         written += iov.length;
       }
-      results->data[0].kind = WASM_I32;
-      results->data[0].of.i32 = written;
-      return NULL;
+      memcpy(data + offset_written, &written, sizeof(int32_t));
+      return wasi_result(results);
     }
 
     // WASI proc_exit: [I32] -> []
     wasm_trap_t* wasi_proc_exit(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
-      FAIL_IF(args->size != 1 || results->size != 0, "Invalid call WASI proc_exit", NULL);
-      Wasm* gw = (Wasm*)env;
-      gw->exit(args->data[0].of.i32);
+      FAIL_IF(args->size != 1 || results->size != 0, "Invalid arguments proc_exit", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      if (!wasm->has_permission("exit")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      wasm->exit(args->data[0].of.i32);
       return NULL;
     }
 
-    wasm_extern_t* factory_wasi_fd_write(wasm_store_t* store, Wasm* gw) {
-      wasm_valtype_t* p[] = { wasm_valtype_new_i32(), wasm_valtype_new_i32(), wasm_valtype_new_i32(), wasm_valtype_new_i32() };
-      wasm_valtype_t* r[] = { wasm_valtype_new_i32() };
-      wasm_valtype_vec_t params, results;
-      wasm_valtype_vec_new(&params, 4, p);
-      wasm_valtype_vec_new(&results, 1, r);
-      wasm_functype_t* type = wasm_functype_new(&params, &results);
-      return wasm_func_as_extern(wasm_func_new_with_env(store, type, wasi_fd_write, gw, NULL));
-    };
+    // WASI args_sizes_get: [I32, I32] -> [I32]
+    wasm_trap_t* wasi_args_sizes_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 2 || results->size != 1, "Invalid arguments args_sizes_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      if (!wasm->has_permission("args")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      byte_t* data = wasm_memory_data(memory);
+      int32_t offset_count = args->data[0].of.i32;
+      int32_t offset_length = args->data[1].of.i32;
+      wasi_encoded_strings encoded = encode_args(CMDLINE_ARGS);
+      memcpy(data + offset_count, &encoded.count, sizeof(int32_t));
+      memcpy(data + offset_length, &encoded.length, sizeof(int32_t));
+      return wasi_result(results);
+    }
 
-    wasm_extern_t* factory_wasi_proc_exit(wasm_store_t* store, Wasm* gw) {
-      wasm_valtype_t* p[] = { wasm_valtype_new_i32() };
-      wasm_valtype_vec_t params, results;
-      wasm_valtype_vec_new(&params, 1, p);
-      wasm_valtype_vec_new_empty(&results);
-      wasm_functype_t* type = wasm_functype_new(&params, &results);
-      return wasm_func_as_extern(wasm_func_new_with_env(store, type, wasi_proc_exit, gw, NULL));
-    };
+    // WASI args_get: [I32, I32] -> [I32]
+    wasm_trap_t* wasi_args_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 2 || results->size != 1, "Invalid arguments args_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      if (!wasm->has_permission("args")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      byte_t* data = wasm_memory_data(memory);
+      int32_t offset_environ = args->data[0].of.i32;
+      int32_t offset_buffer = args->data[1].of.i32;
+      wasi_encoded_strings encoded = encode_args(CMDLINE_ARGS);
+      for (auto i = 0; i < encoded.count; i++) {
+        std::string s = encoded.args[i];
+        memcpy(data + offset_environ, &offset_buffer, sizeof(int32_t));
+        memcpy(data + offset_buffer, s.c_str(), s.length());
+        offset_environ += sizeof(int32_t);
+        offset_buffer += s.length();
+      }
+      return wasi_result(results);
+    }
+
+    // WASI environ_sizes_get: [I32, I32] -> [I32]
+    wasm_trap_t* wasi_environ_sizes_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 2 || results->size != 1, "Invalid arguments environ_sizes_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      byte_t* data = wasm_memory_data(memory);
+      int32_t offset_count = args->data[0].of.i32;
+      int32_t offset_length = args->data[1].of.i32;
+      int32_t zero = 0;
+      memcpy(data + offset_count, &zero, sizeof(int32_t));
+      memcpy(data + offset_length, &zero, sizeof(int32_t));
+      return wasi_result(results);
+    }
+
+    // WASI environ_get: [I32, I32] -> [I32]
+    wasm_trap_t* wasi_environ_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 2 || results->size != 1, "Invalid arguments environ_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      return wasi_result(results);
+    }
+
+    // WASI random_get: [I32, I32] -> [I32]
+    wasm_trap_t* wasi_random_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 2 || results->size != 1, "Invalid arguments random_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      if (!wasm->has_permission("random")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      byte_t* data = wasm_memory_data(memory);
+      int32_t offset = args->data[0].of.i32;
+      int32_t length = args->data[1].of.i32;
+      PackedByteArray bytes = RANDOM_BYTES(length);
+      memcpy(data + offset, BYTE_ARRAY_POINTER(bytes), length);
+      return wasi_result(results);
+    }
+
+    // WASI clock_time_get: [I32, I64, I32] -> [I32]
+    wasm_trap_t* wasi_clock_time_get(void* env, const wasm_val_vec_t* args, wasm_val_vec_t* results) {
+      FAIL_IF(args->size != 3 || results->size != 1, "Invalid arguments clock_time_get", wasi_result(results, __WASI_ERRNO_INVAL, "Invalid arguments\0"));
+      Wasm* wasm = (Wasm*)env;
+      wasm_memory_t* memory = wasm->get_stream().ptr()->memory;
+      if (memory == NULL) return wasi_result(results, __WASI_ERRNO_IO, "Invalid memory\0");
+      if (!wasm->has_permission("time")) return wasi_result(results, __WASI_ERRNO_ACCES, "Not permitted\0");
+      byte_t* data = wasm_memory_data(memory);
+      int32_t clock_id = args->data[0].of.i32;
+      int32_t offset = args->data[2].of.i32;
+      int64_t t = clock_id == __WASI_CLOCKID_REALTIME ? TIME_REALTIME : TIME_MONOTONIC;
+      memcpy(data + offset, &t, sizeof(t));
+      return wasi_result(results);
+    }
+
+    godot_wasm::wasi_callback wasi_factory_factory(const std::vector<wasm_valkind_enum> p_kinds, const std::vector<wasm_valkind_enum> r_kinds, wasm_func_callback_with_env_t c) {
+      auto p_types = new std::vector<wasm_valtype_t*>;
+      auto r_types = new std::vector<wasm_valtype_t*>;
+      for (auto &it: p_kinds) p_types->push_back(wasm_valtype_new(it));
+      for (auto &it: r_kinds) r_types->push_back(wasm_valtype_new(it));
+      wasm_valtype_vec_t params = { p_types->size(), p_types->data() };
+      wasm_valtype_vec_t results = { r_types->size(), r_types->data() };
+      wasm_functype_t* t = wasm_functype_new(&params, &results);
+      return [t, c](wasm_store_t* s, Wasm* w) { return wasm_func_as_extern(wasm_func_new_with_env(s, t, c, w, NULL)); };
+    }
 
     std::map<std::string, godot_wasm::wasi_callback> factories {
-      { "wasi_snapshot_preview1.fd_write", factory_wasi_fd_write },
-      { "wasi_snapshot_preview1.proc_exit", factory_wasi_proc_exit },
+      { "wasi_snapshot_preview1.fd_write", wasi_factory_factory({WASM_I32, WASM_I32, WASM_I32, WASM_I32}, {WASM_I32}, wasi_fd_write) },
+      { "wasi_snapshot_preview1.proc_exit", wasi_factory_factory({WASM_I32}, {}, wasi_proc_exit) },
+      { "wasi_snapshot_preview1.args_sizes_get", wasi_factory_factory({WASM_I32, WASM_I32}, {WASM_I32}, wasi_args_sizes_get) },
+      { "wasi_snapshot_preview1.args_get", wasi_factory_factory({WASM_I32, WASM_I32}, {WASM_I32}, wasi_args_get) },
+      { "wasi_snapshot_preview1.environ_sizes_get", wasi_factory_factory({WASM_I32, WASM_I32}, {WASM_I32}, wasi_environ_sizes_get) },
+      { "wasi_snapshot_preview1.environ_get", wasi_factory_factory({WASM_I32, WASM_I32}, {WASM_I32}, wasi_environ_get) },
+      { "wasi_snapshot_preview1.random_get", wasi_factory_factory({WASM_I32, WASM_I32}, {WASM_I32}, wasi_random_get) },
+      { "wasi_snapshot_preview1.clock_time_get", wasi_factory_factory({WASM_I32, WASM_I64, WASM_I32}, {WASM_I32}, wasi_clock_time_get) },
     };
   }
 
