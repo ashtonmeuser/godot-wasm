5616f1383c3f8f59027ec9cbb4c51ad2ae733c11
Ashton Meuser <ashtonmeuser@gmail.com>
2023-07-10 21:54:02 -0700

    Disable WASI fd write

--- examples/wasm-test/TestWasi.gd
+++ examples/wasm-test/TestWasi.gd
@@ -1,17 +1,17 @@
 extends GodotWasmTestSuite
 
-func test_fd_write():
-	var wasm = load_wasm("wasi")
-	wasm.function("fd_write", [])
-	expect_log("Test fd_write")
-
-#func test_proc_exit():
+#func test_fd_write():
 #	var wasm = load_wasm("wasi")
-#	wasm.function("proc_exit", [0])
-#	expect_log("Module exited successfully")
-#	wasm = load_wasm("wasi")
-#	wasm.function("proc_exit", [1])
-#	expect_error("Module exited with error 1")
+#	wasm.function("fd_write", [])
+#	expect_log("Test fd_write")
+
+func test_proc_exit():
+	var wasm = load_wasm("wasi")
+	wasm.function("proc_exit", [0])
+	expect_log("Module exited successfully")
+	wasm = load_wasm("wasi")
+	wasm.function("proc_exit", [1])
+	expect_error("Module exited with error 1")
 
 #func test_args_get():
 #	var args = get_cmdline_user_args()
@@ -19,12 +19,12 @@ func test_fd_write():
 #	var result = wasm.function("args_get", [])
 #	for arg in args: expect_log(arg.trim_prefix("--"))
 #	expect_eq(result, args.size())
-
+#
 #func test_environ_get():
 #	var wasm = load_wasm("wasi")
 #	var result = wasm.function("environ_get", [])
 #	expect_eq(result, 0)
-
+#
 #func test_random_get():
 #	var wasm = load_wasm("wasi")
 #	var result = wasm.function("random_get", [])
