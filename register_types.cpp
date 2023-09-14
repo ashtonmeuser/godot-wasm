2bebc260c8f42bd5a4f1886fa1e82b220c0171fd
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2023-09-14 08:10:19 -0700

    Migrate to Godot 4.1 (#54)
    * Migrate to Godot 4.1
    
    * Loosen test error log pattern

--- register_types.cpp
+++ register_types.cpp
@@ -22,8 +22,8 @@ void uninitialize_wasm_module(ModuleInitializationLevel p_level) {
 #ifndef GODOT_MODULE
 
 extern "C" {
-  GDExtensionBool GDE_EXPORT wasm_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+  GDExtensionBool GDE_EXPORT wasm_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
     init_obj.register_initializer(initialize_wasm_module);
     init_obj.register_terminator(uninitialize_wasm_module);
