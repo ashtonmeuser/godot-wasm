88785a3b96c57782687b6426f7e2f55370f630aa
Ashton Meuser <ashtonmeuser@Ashtons-MacBook-Pro.local>
2024-06-01 09:29:31 -0700

    Custom void_t type

--- src/string-container.h
+++ src/string-container.h
@@ -12,10 +12,11 @@ Required because List does not support subscript operator, GDExtension PackedStr
 #include "defs.h"
 
 namespace {
+  template <typename...> using void_t = void;
   template <typename, typename = void> struct has_subscript_operator: std::false_type {};
-  template <typename T> struct has_subscript_operator<T, std::__void_t<decltype(std::declval<T&>()[std::declval<size_t>()])>>: std::true_type {};
+  template <typename T> struct has_subscript_operator<T, void_t<decltype(std::declval<T&>()[std::declval<size_t>()])>>: std::true_type {};
   template <typename, typename = void> struct has_get_method: std::false_type {};
-  template <typename T> struct has_get_method<T, std::__void_t<decltype(std::declval<T&>().get(std::declval<size_t>()))>>: std::true_type {};
+  template <typename T> struct has_get_method<T, void_t<decltype(std::declval<T&>().get(std::declval<size_t>()))>>: std::true_type {};
 }
 
 namespace godot {
