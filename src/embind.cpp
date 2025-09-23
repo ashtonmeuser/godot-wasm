7c0d4eb305c2bb61189fdb1f8c413cb24dab9b71
Leandro (cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2025-09-23 12:21:43 -0300

    curr changes

--- src/embind.cpp
+++ src/embind.cpp
@@ -6,5 +6,22 @@ EmBind::EmBind(Wasm *module) {
 	_module = module;
 	_memory = module->get_memory();
 }
+
+void EmBind::__register_bool(uint rawType, uint name, bool trueValue, bool falseValue) {}
+void EmBind::__register_bigint(uint primitiveType, uint name, uint size, uint minRange, uint maxRange) {}
+void EmBind::__register_float(uint rawType, uint name, uint size) {}
+void EmBind::__register_function(uint name, uint argCount, uint rawArgTypesAddr, uint signature, uint rawInvoker, uint fn, bool isAsync, bool isNonnullReturn) {}
+void EmBind::__register_integer(uint primitiveType, uint name, uint size, uint minRange, uint maxRange) {}
+void EmBind::__register_memory_view(uint rawType, uint dataTypeIndex, uint name) {}
+void EmBind::__register_std_string(uint rawType, uint name) {}
+void EmBind::__register_void(uint rawType, uint name) {}
+void EmBind::__abort_js(uint rawType) {}
+void EmBind::__register_emval(uint rawType) {}
+void EmBind::__register_std_wstring(uint rawType, uint charSize, uint name) {}
+void EmBind::__register_std_string(uint rawType, uint charSize, uint name) {}
+void EmBind::__emscripten_resize_heap(uint requestedSize) {}
+void EmBind::__fd_close(uint fd) {}
+void EmBind::__fd_seek(uint fd, uint offset, uint whence, uint newOffset) {}
+
 } //namespace godot_wasm
 } //namespace godot
--- src/embind.h
+++ src/embind.h
@@ -1,8 +1,7 @@
 #ifndef EMBIND_H
 #define EMBIND_H
-#include "core/object/ref_counted.h"
-#include "./wasm-memory.h"
 #include "./wasm.h"
+#include "core/object/ref_counted.h"
 
 namespace godot {
 namespace godot_wasm {
@@ -14,16 +13,21 @@ private:
 public:
 	EmBind(Wasm *module);
 
-	void __embind_register_bool(uint rawType, uint name, bool trueValue, bool falseValue);
-	void __embind_register_bigint(uint primitiveType, uint name, uint size, uint minRange, uint maxRange);
-	void __embind_register_float(uint rawType, uint name, uint size);
-	void __embind_register_function(uint name, uint argCount, uint rawArgTypesAddr, uint signature, uint rawInvoker, uint fn, bool isAsync, bool isNonnullReturn);
-	void __embind_register_integer(uint primitiveType, uint name, uint size, uint minRange, uint maxRange);
-	void __embind_register_memory_view(uint rawType, uint dataTypeIndex, uint name);
-	void __embind_register_std_string(uint rawType, uint name);
-	void __embind_register_void(uint rawType, uint name);
-	void __embind_register_emval(uint rawType);
-	void __embind_register_std_wstring(uint rawType, uint charSize, uint name);
+	void __register_bool(uint rawType, uint name, bool trueValue, bool falseValue);
+	void __register_bigint(uint primitiveType, uint name, uint size, uint minRange, uint maxRange);
+	void __register_float(uint rawType, uint name, uint size);
+	void __register_function(uint name, uint argCount, uint rawArgTypesAddr, uint signature, uint rawInvoker, uint fn, bool isAsync, bool isNonnullReturn);
+	void __register_integer(uint primitiveType, uint name, uint size, uint minRange, uint maxRange);
+	void __register_memory_view(uint rawType, uint dataTypeIndex, uint name);
+	void __register_std_string(uint rawType, uint name);
+	void __register_void(uint rawType, uint name);
+	void __abort_js(uint rawType);
+	void __register_emval(uint rawType);
+	void __register_std_wstring(uint rawType, uint charSize, uint name);
+	void __register_std_string(uint rawType, uint charSize, uint name);
+	void __emscripten_resize_heap(uint requestedSize);
+	void __fd_close(uint fd);
+	void __fd_seek(uint fd, uint offset, uint whence, uint newOffset);
 };
 } //namespace godot_wasm
 } //namespace godot
