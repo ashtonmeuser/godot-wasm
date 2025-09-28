ee4cae1ea7a0c949c1b952e5376bed6fb9d890ea
Leandro (cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2025-09-28 20:06:02 -0300

    fix formatting (somewhat)

--- src/wasi-shim.h
+++ src/wasi-shim.h
@@ -6,23 +6,23 @@
 #include <functional>
 
 // See https://github.com/WebAssembly/wasi-libc/blob/main/libc-bottom-half/headers/public/wasi/api.h
-#define __WASI_CLOCKID_REALTIME (UINT32_C(0)) // The clock measuring real time
+#define __WASI_CLOCKID_REALTIME  (UINT32_C(0)) // The clock measuring real time
 #define __WASI_CLOCKID_MONOTONIC (UINT32_C(1)) // The store-wide monotonic clock
-#define __WASI_ERRNO_SUCCESS (UINT16_C(0)) // No error occurred
-#define __WASI_ERRNO_ACCES (UINT16_C(2)) // [sic] Permission denied
-#define __WASI_ERRNO_INVAL (UINT16_C(28)) // Invalid argument
-#define __WASI_ERRNO_IO (UINT16_C(29)) // I/O error
+#define __WASI_ERRNO_SUCCESS     (UINT16_C(0)) // No error occurred
+#define __WASI_ERRNO_ACCES       (UINT16_C(2)) // [sic] Permission denied
+#define __WASI_ERRNO_INVAL       (UINT16_C(28)) // Invalid argument
+#define __WASI_ERRNO_IO          (UINT16_C(29)) // I/O error
 
 namespace godot {
-class Wasm; // Forward declare to avoid circular dependency
+  class Wasm; // Forward declare to avoid circular dependency
 
-namespace godot_wasm {
-typedef std::tuple<const std::vector<wasm_valkind_enum>, const std::vector<wasm_valkind_enum>, const wasm_func_callback_with_env_t> callback_signature;
+  namespace godot_wasm {
+    typedef std::tuple<const std::vector<wasm_valkind_enum>, const std::vector<wasm_valkind_enum>, const wasm_func_callback_with_env_t> callback_signature;
 
-wasm_func_t *wasi_callback(wasm_store_t *store, Wasm *wasm, callback_signature signature);
-wasm_func_t *get_wasi_callback(wasm_store_t *store, Wasm *wasm, const String name);
-wasm_trap_t *wasi_result(wasm_val_vec_t *results, int32_t value = __WASI_ERRNO_SUCCESS, const char *message = nullptr);
-} //namespace godot_wasm
+    wasm_func_t* wasi_callback(wasm_store_t* store, Wasm* wasm, callback_signature signature);
+    wasm_func_t* get_wasi_callback(wasm_store_t* store, Wasm* wasm, const String name);
+    wasm_trap_t* wasi_result(wasm_val_vec_t* results, int32_t value = __WASI_ERRNO_SUCCESS, const char* message = nullptr);
+  } //namespace godot_wasm
 } //namespace godot
 
 #endif
--- src/wasm.h
+++ src/wasm.h
@@ -12,42 +12,42 @@ namespace godot {
     struct ContextFuncImport;
     struct ContextFuncExport;
     struct ContextMemory;
-  }
+  } //namespace godot_wasm
 
-  class Wasm : public RefCounted {
+  class Wasm: public RefCounted {
     GDCLASS(Wasm, RefCounted);
 
-    private:
-      wasm_module_t* module;
-      wasm_instance_t* instance;
-      godot_wasm::ContextMemory* memory_context;
-      Dictionary permissions;
-      Ref<WasmMemory> memory;
-      std::map<String, godot_wasm::ContextFuncImport> import_funcs;
-      std::map<String, godot_wasm::ContextExtern> export_globals;
-      std::map<String, godot_wasm::ContextFuncExport> export_funcs;
-      void reset_instance();
-      godot_error map_names();
-      wasm_func_t* create_callback(godot_wasm::ContextFuncImport* context);
+  private:
+    wasm_module_t* module;
+    wasm_instance_t* instance;
+    godot_wasm::ContextMemory* memory_context;
+    Dictionary permissions;
+    Ref<WasmMemory> memory;
+    std::map<String, godot_wasm::ContextFuncImport> import_funcs;
+    std::map<String, godot_wasm::ContextExtern> export_globals;
+    std::map<String, godot_wasm::ContextFuncExport> export_funcs;
+    void reset_instance();
+    godot_error map_names();
+    wasm_func_t* create_callback(godot_wasm::ContextFuncImport* context);
 
-    public:
-      static void REGISTRATION_METHOD();
-      Wasm();
-      ~Wasm();
-	  void _init();
+  public:
+    static void REGISTRATION_METHOD();
+    Wasm();
+    ~Wasm();
+    void _init();
 
-      void exit(int32_t code);
-      godot_error compile(PackedByteArray bytecode);
-      godot_error instantiate(const Dictionary import_map);
-      godot_error load(PackedByteArray bytecode, const Dictionary import_map);
-      Dictionary inspect() const;
-      Variant function(String name, Array args) const;
-      Variant global(String name) const;
-      Ref<WasmMemory> get_memory() const;
-      void set_permissions(const Dictionary &update);
-      Dictionary get_permissions() const;
-      bool has_permission(String permission) const;
+    void exit(int32_t code);
+    godot_error compile(PackedByteArray bytecode);
+    godot_error instantiate(const Dictionary import_map);
+    godot_error load(PackedByteArray bytecode, const Dictionary import_map);
+    Dictionary inspect() const;
+    Variant function(String name, Array args) const;
+    Variant global(String name) const;
+    Ref<WasmMemory> get_memory() const;
+    void set_permissions(const Dictionary &update);
+    Dictionary get_permissions() const;
+    bool has_permission(String permission) const;
   };
-}
+} //namespace godot
 
 #endif
