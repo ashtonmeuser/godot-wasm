16e765ab048b1b1ffd1f23c077cd309eecb2c6d2
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2025-10-01 16:51:36 -0700

    Update tests

--- examples/wasm-test/TestWasi.gd
+++ examples/wasm-test/TestWasi.gd
@@ -43,28 +43,11 @@ func test_clock_time_get():
 	var result = wasm.function("clock_time_get", [])
 	expect_within(result, time, 1000.0) # Within one second
 
-func test_permissions():
-	var wasm = load_wasm("wasi")
-	expect_includes(wasm.permissions, "print")
-	expect_eq(wasm.permissions.get("print"), true)
-	wasm.permissions = { "print": false }
-	expect_eq(wasm.permissions.get("print"), false)
-	var permission = wasm.has_permission("print")
-	expect_eq(permission, false)
-	wasm.permissions = { "print": true }
-	permission = wasm.has_permission("print")
-	expect_eq(permission, true)
-
-func test_invalid_permissions():
-	# TODO: No exit permissions causes crash on proc_exit
-	# TODO: Args & env not affected by permissions
-	var wasm = load_wasm("wasi")
-	wasm.permissions = { "print": false }
-	wasm.function("fd_write", [])
-	expect_error("Failed calling function fd_write")
-	wasm.permissions = { "print": true, "random": false }
-	wasm.function("random_get", [])
-	expect_error("Failed calling function random_get")
-	wasm.permissions = { "random": true, "time": false }
-	wasm.function("clock_time_get", [])
-	expect_error("Failed calling function clock_time_get")
+func test_disable_extension():
+	var wasm = Wasm.new()
+	var buffer = read_file("wasi")
+	expect_includes(wasm.extensions, "wasi_preview1")
+	wasm.extensions = [] # Remove all extensions
+	var error = wasm.load(buffer, {})
+	expect_eq(error, ERR_CANT_CREATE)
+	expect_error("Missing import function wasi_snapshot_preview1.args_get")
